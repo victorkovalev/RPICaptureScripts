86383c593ba86170ff1f24521f25532f7d02d55e
victorkovalev <vkovalev@rpic.com>
2017-01-04 09:28:49 -0700

    Initial Add of the functions

---
+++ fnGetValueInFieldList.vb
@@ -0,0 +1,77 @@
+﻿''' <summary>
+'''     Function will take in the a string and do a fuzzy match to a list in the field. 
+'''         > fnGetValueInFieldList("Class1", "Country", "Peru1", 0.2) -> "Peru"
+'''     Spaces are removed for comparison.
+'''         > fnGetValueInFieldList("Class1", "Country", "P e r u", 0.2) -> "Peru"
+'''     Spaces are removed for comparison.
+'''         > fnGetValueInFieldList("Class1", "Country", "Peruza", 0.2) -> ""
+''' </summary>
+''' <remarks>Requires Logging Functions, Cedar String Compare library</remarks>
+''' <param name="strClassName">Class name where the validation list is defined</param>
+''' <param name="strFieldName">Field name where the validation list is present</param>
+''' <param name="strValue">String value to match</param>
+''' <param name="sglDistance">Distance allowed between the text that we enter and an item found in the list</param>
+''' <returns>"" if no match is found with tolerance provided, Otherwise item that matches the list</returns>
+
+Function fnGetValueInFieldList(strClassName As String, strFieldName As String, strValue As String, sglDistance As Single) As String
+    On Error GoTo lbl_error
+
+    fnLog(CDRTypeInfo, "fnGetValueInFieldList Start strClassName [" & strClassName & "] strFieldName [" & strFieldName & "] strValue [" & strValue & "]")
+    fnLogIndentIncrease()
+
+    fnGetValueInFieldList = ""
+
+    ' Get list settings
+    Dim oIVRSettings As SCBCdrListSettings
+    Dim oObject As Object
+    Dim oIVRFieldDef As SCBCdrFieldDef
+
+    Set oIVRFieldDef = Project.AllClasses(strClassName).Fields.ItemByName(strFieldName)
+    Set oObject = oIVRFieldDef.ValidationSettings("German")
+    Set oIVRSettings = oObject
+
+    ' Initialize compare settings
+    Dim sglDist As Single
+    Dim oStrComp As SCBCdrStringComp
+    Set oStrComp = New SCBCdrStringComp
+
+    oStrComp.CompType = CdrTypeLevenShtein
+    oStrComp.CaseSensitive = False
+    oStrComp.SearchExpression = Replace(strValue, " ", "") 'Compare without spaces
+
+    ' Variables to store best candidates
+    Dim lngItem As Long
+    Dim lngBestItem As Long
+    Dim sglBestItemDistance As Single
+
+    lngBestItem = 0
+    sglBestItemDistance = 1
+
+    ' Iterate through the list, and compare each item
+    For lngItem = 1 To oIVRFieldDef.ListItemCount
+        'Compare without spaces
+        oStrComp.Distance(Replace(oIVRSettings.ListValues.Item(lngItem), " ", ""), sglDist)
+        fnLog(CDRTypeInfo, "fnGetValueInFieldList lngItem [" & CStr(lngItem) & "] itemText [" & oIVRSettings.ListValues.Item(lngItem) & "] sglDist [" & CStr(sglDist) & "]")
+
+        'If match is better, store it
+        If (sglDist < sglBestItemDistance) Then
+            lngBestItem = lngItem
+            sglBestItemDistance = sglDist
+        End If
+
+    Next lngItem
+
+    ' Log the best candidate
+    fnLog(CDRTypeInfo, "fnGetValueInFieldList lngBestItem [" & CStr(lngBestItem) & "] strBestItemText [" & oIVRSettings.ListValues.Item(lngBestItem) & "] sglBestItemDistance [" & CStr(sglBestItemDistance) & "]")
+
+    ' Return text of best candidate if item in the list is within tolerance
+    If (sglBestItemDistance <= sglDistance) Then fnGetValueInFieldList = oIVRSettings.ListValues.Item(lngBestItem)
+
+lbl_end:
+    fnLogIndentDecrease()
+    fnLog(CDRTypeInfo, "fnGetValueInFieldList End Returning [" & CStr(fnGetValueInFieldList) & "]")
+    Exit Function
+lbl_error:
+    fnLogIndentDecrease()
+    fnLog(CDRTypeError, "fnGetValueInFieldList Error [" & Err.Description & "]")
+End Function
---
+++ fnLog.vb
@@ -0,0 +1,102 @@
+﻿''' <summary>
+'''     Logging Functions, helpfull to make shorter calls from scripts and support indenting.
+'''     
+'''     Sample Function:
+'''         Function fnGetValueInFieldList(strClassName As String, strFieldName As String, strValue As String, sglDistance As Single) As String
+'''             On Error GoTo lbl_error
+'''         
+'''             fnLog(CDRTypeInfo, "fnGetValueInFieldList Start strClassName [" & strClassName & "] strFieldName [" & strFieldName & "] strValue [" & strValue & "]")
+'''             fnLogIndentIncrease()
+'''             
+'''             'DO Some work
+'''         lbl_end:
+'''             fnLogIndentDecrease()
+'''             fnLog(CDRTypeInfo, "fnGetValueInFieldList End Returning [" & CStr(fnGetValueInFieldList) & "]")
+'''             Exit Function
+'''         lbl_error:
+'''             fnLogIndentDecrease()
+'''             fnLog(CDRTypeError, "fnGetValueInFieldList Error [" & Err.Description & "]")
+'''         End Function
+''' </summary>
+''' <remarks>Global variables to store the level of indent with logging</remarks>
+Global gLogIndent As Integer
+Global gLogSuspend As Boolean
+
+''' <summary>
+'''     Function can be used to suspend verbose logging.
+'''     For example when loading configuration, you can have a call to fnLogSuspend to pause logging, then resume when load is complete
+''' </summary>
+''' <param name="pLogSuspend">Boolean true/false</param>
+Public Function fnLogSuspend(pLogSuspend As Boolean)
+    On Error GoTo lbl_error
+    gLogSuspend = pLogSuspend
+
+    Exit Function
+lbl_error:
+    fnLog(CDRTypeError, "fnLogSuspend Error [" & Err.Description & "]")
+End Function
+
+''' <summary>
+'''     Resets the log indent level to 0
+''' </summary>
+Public Function fnLogIndentReset()
+    On Error GoTo lbl_error
+    gLogIndent = 0
+
+    Exit Function
+lbl_error:
+    fnLog(CDRTypeError, "fnLogIndentReset Error [" & Err.Description & "]")
+End Function
+
+''' <summary>
+'''     Increases the log indent by 1 level (2 spaces)
+''' </summary>
+Public Function fnLogIndentIncrease()
+    On Error GoTo lbl_error
+    gLogIndent = gLogIndent + 1
+
+    If gLogIndent > 20 Then
+        gLogIndent = 20
+    End If
+
+    Exit Function
+lbl_error:
+    fnLog(CDRTypeError, "fnLogIndentIncrease Error [" & Err.Description & "]")
+End Function
+
+''' <summary>
+'''     Decreases the log indent by 1 level (2 spaces)
+''' </summary>
+Public Function fnLogIndentDecrease()
+    On Error GoTo lbl_error
+    gLogIndent = gLogIndent - 1
+
+    If gLogIndent < 0 Then
+        gLogIndent = 0
+    End If
+
+    Exit Function
+lbl_error:
+    fnLog(CDRTypeError, "fnLogIndentDecrease Error [" & Err.Description & "]")
+End Function
+
+''' <summary>
+'''     Logs the message at specific level (CDRTypeError, CDRTypeWarning, CDRTypeInfo)
+''' </summary>
+''' <param name="pType">CDRTypeError, CDRTypeWarning, CDRTypeInfo</param>
+''' <param name="pMessage">Message to print</param>
+Public Function fnLog(pType As CDRMessageType, pMessage As String)
+    On Error GoTo lbl_error
+
+    ' Do not log info messages if logging is suspended
+    If gLogSuspend = True And pType = CDRMessageType.CDRTypeInfo Then
+        GoTo lbl_end
+    End If
+
+    Project.LogScriptMessageEx(pType, CDRSeverityLogFileOnly, Space(gLogIndent * 2) & pMessage)
+
+lbl_end:
+    Exit Function
+lbl_error:
+    Project.LogScriptMessageEx(CDRTypeError, CDRSeverityLogFileOnly, "fnLogIndentDecrease Error [" & Err.Description & "]")
+End Function
